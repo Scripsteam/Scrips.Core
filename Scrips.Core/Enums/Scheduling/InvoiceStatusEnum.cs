ee559e77c023bb73dafca8ff1bff150f13a2226b
prince.kumar <prince.kumar@SMARTDATA-128>
2022-01-20 19:51:54 +0530

    MSP-6761

---
+++ Scrips.Core/Enums/Scheduling/InvoiceStatusEnum.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Scheduling
+{
+    /// <summary>
+    /// Invoice Enum
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum InvoiceStatusEnum
+    {
+        [Display(Name = "Draft")]
+        Draft,
+
+        [Display(Name = "Issued")]
+        Issued,
+
+        [Display(Name = "Balanced")]
+        Balanced,
+
+        [Display(Name = "Canceled")]
+        Canceled,
+
+        [Display(Name = "Claimed")]
+        Claimed,
+
+        [Display(Name = "Reclaimed")]
+        Reclaimed
+    }
+}
