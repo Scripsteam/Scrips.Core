c3d0e0e623f724853c9982cf583bdbdd61d30799
yura <bartish.yura@gmail.com>
2022-01-21 18:10:35 +0200

    Add sponsor type enum

---
+++ Scrips.Core/Enums/Billing/SponsorType.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Billing
+{
+    /// <summary>
+    /// Sponsor Type Enum
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SponsorType
+    {
+        /// <summary>
+        /// Self-Pay Type
+        /// </summary>
+        [Display(Name = "Self-Pay")]
+        SelfPay,
+
+        /// <summary>
+        /// Insurance Type
+        /// </summary>
+        [Display(Name = "Insurance")]
+        Insurance,
+
+        /// <summary>
+        /// Corporate Type
+        /// </summary>
+        [Display(Name = "Corporate")]
+        Corporate
+    }
+}
