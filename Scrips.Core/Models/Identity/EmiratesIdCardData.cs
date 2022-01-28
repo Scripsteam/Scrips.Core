bc2aec07152c97cb95544cfbfb81db1b8303ad81
Andrew Safwat <andrew@scrips.com>
2022-01-28 18:05:43 +0200

    add EmiratesIdCardData model

---
+++ Scrips.Core/Models/Identity/EmiratesIdCardData.cs
@@ -0,0 +1,13 @@
+﻿namespace Scrips.Core.Models.EmirateId
+{
+    public class EmiratesIdCardData
+    {
+        public string IdNumber { get; set; }
+        public string CardNumber { get; set; }
+        public string FullNameArabic { get; set; }
+        public string FullNameEnglish { get; set; }
+        public string ExpiryDate { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
