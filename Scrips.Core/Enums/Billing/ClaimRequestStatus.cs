5364e9625c1659ce66dfb441ac8f4d44e8a6a2c4
yura-b <barish.yura@gmail.com>
2022-04-22 19:20:15 +0300

    Add enums of  claim statuses

---
+++ Scrips.Core/Enums/Billing/ClaimRequestStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Billing
+{
+    /// <summary>
+    /// Invoice Claim Request Status
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ClaimRequestStatus
+    {
+        [Display(Name = "Sending")]
+        Sending,
+
+        [Display(Name = "Submitted")]
+        Submitted,
+
+        [Display(Name = "Error")]
+        Error,
+
+        [Display(Name = "Connection Error")]
+        ConnectionError,
+
+        [Display(Name = "Ready for Submission")]
+        ReadyForSubmission,
+
+        [Display(Name = "Denial Addressed")]
+        DenialAddressed
+    }
+}
---
+++ Scrips.Core/Enums/Billing/ClaimStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Billing
+{
+    /// <summary>
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ClaimStatusType
+    {
+        [Display(Name = "Initial")]
+        Initial,
+
+        [Display(Name = "Hold")]
+        Hold,
+
+        [Display(Name = "Reviewed")]
+        Reviewed,
+
+        [Display(Name = "Submitted")]
+        Submitted,
+
+        [Display(Name = "Resubmitted")]
+        Resubmitted,
+
+        [Display(Name = "Remitted")]
+        Remitted,
+
+        [Display(Name = "Revised & Reviewed")]
+        RevisedAndReviewed,
+
+        [Display(Name = "Reconciled")]
+        Reconciled,
+
+        [Display(Name = "Denial Addressed")]
+        DenialAddressed
+    }
+}
