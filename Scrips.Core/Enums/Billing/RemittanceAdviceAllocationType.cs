ecb61f22bfd1fdd2f5f5e9edfa5440626f188789
yura-b <barish.yura@gmail.com>
2022-05-10 20:54:03 +0300

    Add enums for RemittanceAdvice

---
+++ Scrips.Core/Enums/Billing/RemittanceAdviceAllocationType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Billing
+{
+    /// <summary>
+    /// Remittance Advice Allocation Type Enum
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RemittanceAdviceAllocationType
+    {
+
+        [Display(Name = "Auto-allocated")]
+        AutoAllocated,
+
+        [Display(Name = "File")]
+        File
+    }
+}
---
+++ Scrips.Core/Enums/Billing/RemittanceAdviceStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Scrips.Core.Enums.Billing
+{
+    /// <summary>
+    /// Remittance Advice Status Enum
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RemittanceAdviceStatus
+    {
+        [Display(Name = "Pending")]
+        Pending,
+
+        [Display(Name = "Processed")]
+        Processed
+    }
+}
