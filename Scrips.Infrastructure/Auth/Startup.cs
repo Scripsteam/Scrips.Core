c26db1521e6f1ee9b7961be0c4a9b84176a50518
Amr Elsehemy <me@amrelsehemy.net>
2022-09-18 10:09:50 +0200

    Adding Scrips.Infrastructure to Core

---
+++ Scrips.Infrastructure/Auth/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Scrips.Core.Application.Common.Interfaces;
+
+namespace Scrips.Infrastructure.Auth;
+internal static class Startup
+{
+    internal static IApplicationBuilder UseCurrentUser(this IApplicationBuilder app) =>
+        app.UseMiddleware<CurrentUserMiddleware>();
+
+
+    internal static IServiceCollection AddCurrentUser(this IServiceCollection services) =>
+        services
+            .AddScoped<CurrentUserMiddleware>()
+            .AddScoped<ICurrentUser, CurrentUser>()
+            .AddScoped(sp => (ICurrentUserInitializer)sp.GetRequiredService<ICurrentUser>());
+}
---
+++ Scrips.Infrastructure/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrips.Infrastructure.Auth;
+
+namespace Scrips.Infrastructure;
+public static class Startup
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        return services
+
+            // .AddApiVersioning()
+            .AddCurrentUser()
+
+            // .AddBackgroundJobs(config)
+            // .AddCaching(config)
+            // .AddCorsPolicy(config)
+            // .AddExceptionMiddleware()
+            // .AddHealthCheck()
+            // .AddPOLocalization(config)
+            // .AddMailing(config)
+            // .AddMediatR(Assembly.GetExecutingAssembly())
+            // .AddMultitenancy(config)
+            // .AddNotifications(config)
+            // .AddOpenApiDocumentation(config)
+            // .AddPersistence(config)
+            // .AddRequestLogging(config)
+            // .AddRouting(options => options.LowercaseUrls = true)
+            // .AddServices()
+            ;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder builder) =>
+            builder
+
+                // .UseRequestLocalization()
+                // .UseStaticFiles()
+                // .UseSecurityHeaders(config)
+                // .UseFileStorage()
+                // .UseExceptionMiddleware()
+                // .UseRouting()
+                // .UseCorsPolicy()
+                // .UseAuthentication()
+                .UseCurrentUser()
+
+                // .UseMultiTenancy()
+                // .UseAuthorization()
+                // .UseRequestLogging(config)
+                // .UseHangfireDashboard(config)
+                // .UseOpenApiDocumentation(config)
+                ;
+}
