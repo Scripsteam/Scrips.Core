50c653a41f3f11413572b382ede38036bbddbbce
Amr Elsehemy <me@amrelsehemy.net>
2022-11-14 09:17:07 +0200

    adding PracticeSetup

---
+++ Scrips.Core/Models/Practice/PracticeWorkingHourList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Scrips.Core.Models.Practice
+{
+    public class PracticeWorkingHoursDetailsModel
+    {
+        public string WeekDayId { get; set; }
+        public string StartHour { get; set; }
+        public string StartMinute { get; set; }
+        public string EndHour { get; set; }
+        public string EndMinute { get; set; }
+        public Guid PracticeId { get; set; }
+    }
+}
