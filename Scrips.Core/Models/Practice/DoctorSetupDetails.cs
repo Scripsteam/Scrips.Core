d1b47f341e986599fdaa03890743907e91d82a36
Amr Elsehemy <me@amrelsehemy.net>
2022-11-17 13:13:20 +0200

    add models

---
+++ Scrips.Core/Models/Practice/DoctorSetupDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrips.Core.Models.Practice;
+
+public class DoctorSetupDetails
+{
+    public Guid OrganizationId { get; set; }
+    public Guid DoctorSetupId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailId { get; set; }
+
+    public bool IsSuperAdmin { get; set; }
+    public string SpecialityName { get; set; }
+    public Guid SpecialityId { get; set; }
+    public int ServiceCategoryId { get; set; }
+    public int ServiceTypeId { get; set; }
+    public bool? InvitationStatus { get; set; }
+    public List<Doctors> PracticeList { get; set; }
+
+    public DateTime? IsLastLogin { get; set; }
+
+    public Speciality Speciality { get; set; }
+    public string PhotoURL { get; set; }
+    public string PracticeName { get; set; }
+}
+public class Doctors
+{
+    public string PracticeName { get; set; }
+}
