f07e18172d182989d9d46f3cee267d00277bd697
Amr Elsehemy <me@amrelsehemy.net>
2022-11-19 23:57:08 +0200

    add return model

---
+++ Scrips.Core/Models/Provider/ProviderListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application.Abstractions.Dtos
+{
+    public class ProviderListModel
+    {
+        public Guid Id { get; set; }
+        public Guid? OrganizationId { get; set; }
+        public Guid? PracticeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Dob { get; set; }
+        public Guid Genderid { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
