fbc6e6d65d083e8d7e1e5885f316cfa9c8681929
Amr Elsehemy <me@amrelsehemy.net>
2022-11-23 13:09:43 +0200

    response

---
+++ Scrips.Core/Models/Practice/StaffListResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Scrips.Core.Models.Practice
+{
+    public class StaffListResponse
+    {
+        public string RoleName { get; set; }
+        public List<StaffSetupDetails> StaffList { get; set; }
+    }
+}
