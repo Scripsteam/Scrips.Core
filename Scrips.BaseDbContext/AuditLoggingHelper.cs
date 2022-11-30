124d027c3f7bcd9e3617fda8d0be19c8f3bf3cf7
Sercan Dagdas <sercan@scrips.com>
2022-11-30 13:02:10 +0300

    Added auditable multi-tenant db context

---
+++ Scrips.BaseDbContext/AuditLoggingHelper.cs
@@ -0,0 +1,77 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using Scrips.BaseDbContext.Entities;
+using Scrips.Core.Models.Audit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Scrips.BaseDbContext
+{
+    public class AuditLoggingHelper
+    {
+        public static List<LogAudit> DetectChanges(ChangeTracker changeTracker, IHttpContextAccessor _httpContextAccessor)
+        {
+            //Get all changed records excluding Audit records themselves
+            var changes = changeTracker.Entries()
+                    .Where(x => x.State != EntityState.Unchanged && x.State != EntityState.Detached && x.Entity is not LogAudit).ToList();
+
+            //Get the user and other related identities, preferred to have an extension methods like GetUserId(), GetTenantId(), ...
+            var user = _httpContextAccessor?.HttpContext?.User?.FindFirst(JwtClaimTypes.Subject)?.Value;
+            var tenant = _httpContextAccessor?.HttpContext?.User?.FindFirst("tenant")?.Value;
+
+            string ip = _httpContextAccessor?.HttpContext.Request.Headers["ipaddress"];
+
+            //list of logs to be saved
+            var logs = new List<LogAudit>();
+            var maskedValue = "*";//default value to save instead of masked
+            var maskValue = false;
+
+            foreach (var change in changes)
+            {
+                var entry = new AuditEntityEntry();
+                if (user is not null)//will there be changes without user like signup!?
+                    entry.User = Guid.Parse(user);
+                entry.Entity = change.Entity.GetType().Name;
+                entry.Ip = ip;
+                entry.Tenant = tenant;
+
+                foreach (var prop in change.Properties)
+                {
+                    maskValue = false;
+                    if (prop.Metadata.PropertyInfo.GetCustomAttribute<MaskValueAuditAttribute>() is not null)
+                        maskValue = true;
+
+                    if (prop.Metadata.IsPrimaryKey())
+                        entry.KeyValues[prop.Metadata.Name] = prop.CurrentValue;
+                    else switch (change.State)
+                        {
+                            case EntityState.Deleted:
+                                entry.AuditActionType = AuditActionType.Deleted;
+                                entry.OldValues[prop.Metadata.Name] = maskValue ? maskedValue : prop.OriginalValue;
+                                break;
+                            case EntityState.Added:
+                                entry.AuditActionType = AuditActionType.Added;
+                                entry.NewValues[prop.Metadata.Name] = maskValue ? maskedValue : prop.CurrentValue;
+                                break;
+                            case EntityState.Modified:
+                                if (prop.IsModified && prop.OriginalValue != prop.CurrentValue)
+                                {
+                                    entry.AuditActionType = AuditActionType.Updated;
+                                    entry.OldValues[prop.Metadata.Name] = maskValue ? maskedValue : prop.OriginalValue;
+                                    entry.NewValues[prop.Metadata.Name] = maskValue ? maskedValue : prop.CurrentValue;
+                                }
+                                break;
+                        }
+                }
+                logs.Add(entry.ToLogAudit());
+            }
+            return logs;
+        }
+    }
+}
