3e249f4087f5d468dec31552406002ac90a24440
Amr Elsehemy <me@amrelsehemy.net>
2023-01-08 20:56:34 +0200

    adding models

---
+++ Scrips.Core/Models/Provider/ProviderReasonVisitModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Scrips.Core.Models.Provider
+{
+    public class ProviderReasonVisitModel
+    {
+        public Guid? Id { get; set; }
+        public string Code { get; set; }
+        public string Display { get; set; }
+        public string System { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class ProviderReasonCodeModel
+    {
+        /// <summary>
+        /// provider id is used as Code id
+        /// </summary>        
+        public string Code { get; set; }
+
+        /// <summary>
+        /// An explanation of the meaning of the concept
+        /// </summary>
+        public string Definition { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>     
+        public string Display { get; set; }
+    }
+}
---
+++ Scrips.Core/Models/Provider/SpecialitySkillsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scrips.Core.Models.Provider
+{
+    public class SpecialitySkillsModel
+    {
+       
+        public Guid? Id { get; set; }
+        public Guid SpecialitySkillId { get; set; }
+        public CodeModel Code { get; set; }
+    }
+}
