3ae8915b389656c8bd99b39400f3ba00f5ae3878
Sejal Patel <sejal@scrips.com>
2023-02-14 16:11:38 +0530

    Added new topics

--- Scrips.Core/Topics.cs
+++ Scrips.Core/Topics.cs
@@ -7,4 +7,6 @@ public static class Topics
     public const string DoctorCreated = nameof(DoctorCreated);
 
     public const string OrganizationCreated = nameof(OrganizationCreated);
+
+    public const string PracticeCreated = nameof(PracticeCreated);
 }
