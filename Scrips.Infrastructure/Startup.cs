0f391bc0bcf30104302ecc58d323c85ceb383209
Amr Elsehemy <amr@scrips.com>
2023-02-17 13:32:08 +0200

    fix mediatR

--- Scrips.Infrastructure/Startup.cs
+++ Scrips.Infrastructure/Startup.cs
@@ -8,26 +8,28 @@ using Scrips.Infrastructure.Persistence;
 using System.Reflection;
 
 namespace Scrips.Infrastructure;
+
 public static class Startup
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
         return services
-            .AddApiVersioning()
-            .AddCurrentUser()
+                .AddApiVersioning()
+                .AddCurrentUser()
 
-            // .AddBackgroundJobs(config)
-            // .AddCaching(config)
-            // .AddCorsPolicy(config)
-            // .AddExceptionMiddleware()
-            // .AddHealthCheck()
-            .AddLocalization()
-            // .AddMailing(config)
-            .AddMediatR(Assembly.GetExecutingAssembly())
-            // .AddMultitenancy(config)
-            // .AddNotifications(config)
-            // .AddOpenApiDocumentation(config)
-            .AddPersistence(config)
+                // .AddBackgroundJobs(config)
+                // .AddCaching(config)
+                // .AddCorsPolicy(config)
+                // .AddExceptionMiddleware()
+                // .AddHealthCheck()
+                .AddLocalization()
+                // .AddMailing(config)
+                //.AddMediatR(Assembly.GetExecutingAssembly())
+                .AddMediatR(cfg => { cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()); })
+                // .AddMultitenancy(config)
+                // .AddNotifications(config)
+                // .AddOpenApiDocumentation(config)
+                .AddPersistence(config)
             // .AddRequestLogging(config)
             //.AddRouting(options => options.LowercaseUrls = true)
             // .AddServices()
@@ -35,30 +37,30 @@ public static class Startup
     }
 
     public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder builder) =>
-            builder
+        builder
 
-                // .UseRequestLocalization()
-                // .UseStaticFiles()
-                // .UseSecurityHeaders(config)
-                // .UseFileStorage()
-                // .UseExceptionMiddleware()
-                // .UseRouting()
-                // .UseCorsPolicy()
-                // .UseAuthentication()
-                .UseCurrentUser()
+            // .UseRequestLocalization()
+            // .UseStaticFiles()
+            // .UseSecurityHeaders(config)
+            // .UseFileStorage()
+            // .UseExceptionMiddleware()
+            // .UseRouting()
+            // .UseCorsPolicy()
+            // .UseAuthentication()
+            .UseCurrentUser()
 
-                // .UseMultiTenancy()
-                // .UseAuthorization()
-                // .UseRequestLogging(config)
-                // .UseHangfireDashboard(config)
-                // .UseOpenApiDocumentation(config)
-                ;
+    // .UseMultiTenancy()
+    // .UseAuthorization()
+    // .UseRequestLogging(config)
+    // .UseHangfireDashboard(config)
+    // .UseOpenApiDocumentation(config)
+    ;
 
     private static IServiceCollection AddApiVersioning(this IServiceCollection services) =>
-    services.AddApiVersioning(config =>
-    {
-        config.DefaultApiVersion = new ApiVersion(1, 0);
-        config.AssumeDefaultVersionWhenUnspecified = true;
-        config.ReportApiVersions = true;
-    });
+        services.AddApiVersioning(config =>
+        {
+            config.DefaultApiVersion = new ApiVersion(1, 0);
+            config.AssumeDefaultVersionWhenUnspecified = true;
+            config.ReportApiVersions = true;
+        });
 }
