9ff7279e34b20d261a6462f826ec30182829a081
Amr Elsehemy <amr@scrips.com>
2023-02-19 12:43:18 +0200

    expose Ipersonapi

---
+++ Scrips.Core/Models/Person/PersonInfoResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Scrips.Core.Models.Person;
+
+public class PersonInfoResponse
+{
+    public Guid OrganizationId { get; set; }
+    public Guid PracticeId { get; set; }
+
+    public Guid PatientId { get; set; }
+
+    public string OrganizationName { get; set; }
+    public string BrandingUrl { get; set; }
+
+    public string ImageURL { get; set; }
+
+    public bool Status { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Age { get; set; }
+}
