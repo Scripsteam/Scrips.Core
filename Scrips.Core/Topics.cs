167b9a8931c7e24451509921ca78974162794b2e
Sercan Dagdas <sercan@scrips.com>
2023-02-20 09:41:52 +0300

    added topics

--- Scrips.Core/Topics.cs
+++ Scrips.Core/Topics.cs
@@ -8,8 +8,12 @@ public static class Topics
 
     public const string OrganizationCreated = nameof(OrganizationCreated);
     public const string OrganizationV1Created = nameof(OrganizationV1Created);
+    public const string OrganizationV1Updated = nameof(OrganizationV1Updated);
 
     public const string PracticeCreated = nameof(PracticeCreated);
-
     public const string PracticeUpdated = nameof(PracticeUpdated);
+
+    public const string OrganizationSettingsCreated = nameof(OrganizationSettingsCreated);
+    public const string OrganizationSettingsUpdated = nameof(OrganizationSettingsUpdated);
+
 }
