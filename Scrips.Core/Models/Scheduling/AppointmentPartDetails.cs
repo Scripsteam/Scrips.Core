1e9884946dc93d328761e2407a451986d8385826
Andrew Safwat <andrew@scrips.com>
2023-09-20 12:58:20 +0300

    add check for create blockSlot

---
+++ Scrips.Core/Models/Scheduling/AppointmentPartDetails.cs
@@ -0,0 +1,23 @@
+﻿using Scrips.Core.Models.Practice;
+
+namespace Scrips.Core.Models.Scheduling
+{
+    public class AppointmentPartDetails
+    {
+        public Guid Id { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public AppointmentStatus Status { get; set; }
+        public AppointmentType AppointmentType { get; set; }
+        public LocationModel Location { get; set; }
+        public ReasonCode ReasonCode { get; set; }
+        public PatientDetails Patient { get; set; }
+    }
+
+    public class PatientDetails
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
