d873e916e9a25e6a0ce98e2daa885e62e548d3ed
Andrew Safwat <andrew@scrips.com>
2024-02-13 16:26:11 +0200

    handle corporate calculation

--- Scrips.Core/Models/Billing/ClonedBillingGroupDto.cs
+++ Scrips.Core/Models/Billing/ClonedBillingGroupDto.cs
@@ -4,23 +4,46 @@ public class ClonedBillingGroupDto
 {
     public Guid BillingGroupId { get; set; }
     public decimal NetSum { get; set; }
+    public bool IsDiscount { get; set; } = false;
     public bool CoPayIsFixed { get; set; }
     public bool CoPayIsPercent { get; set; }
     public decimal TotalPatientShare => GetTotalPatientShare();
+    public decimal TotalPatientDiscount => GetTotalPatientDiscount();
     public decimal CoPayValue { get; set; }
     public decimal? MaximumCopaymentValue { get; set; }
 
     private decimal GetTotalPatientShare()
     {
-        if (CoPayIsFixed)
+        if (!IsDiscount)
         {
-            var copay = Math.Min(NetSum, CoPayValue);
-            return MaximumCopaymentValue.HasValue ? Math.Min(copay, MaximumCopaymentValue.Value) : copay;
+            if (CoPayIsFixed)
+            {
+                var copay = Math.Min(NetSum, CoPayValue);
+                return MaximumCopaymentValue.HasValue ? Math.Min(copay, MaximumCopaymentValue.Value) : copay;
+            }
+            else if (CoPayIsPercent)
+            {
+                var copay = CoPayValue * NetSum / 100;
+                return MaximumCopaymentValue.HasValue ? Math.Min(copay, MaximumCopaymentValue.Value) : copay;
+            }
         }
-        else if (CoPayIsPercent)
+        return 0;
+    }
+
+    private decimal GetTotalPatientDiscount()
+    {
+        if (IsDiscount)
         {
-            var copay = CoPayValue * NetSum / 100;
-            return MaximumCopaymentValue.HasValue ? Math.Min(copay, MaximumCopaymentValue.Value) : copay;
+            if (CoPayIsFixed)
+            {
+                var copay = NetSum - CoPayValue;
+                return copay > 0 ? copay : 0;
+            }
+            else if (CoPayIsPercent)
+            {
+                var copay = CoPayValue * NetSum / 100;
+                return copay > 0 ? copay : 0;
+            }
         }
         return 0;
     }
