4776dbbcca597032e23754debbf1d38217de9b62
Andrew Safwat <andrew@scrips.com>
2024-06-01 13:18:55 +0300

    undo calling new api

--- Scrips.Core/HttpApiClients/IPracticeApi.cs
+++ Scrips.Core/HttpApiClients/IPracticeApi.cs
@@ -25,7 +25,7 @@ public interface IPracticeApi
     [Get("/api/Practice/PracticeSetupDetails/{practiceId}")]
     Task<PracticeDetailsResponse> PracticeSetupDetails(Guid practiceId, [Header("Authorization")] string token);
 
-    [Post("/api/Doctor/CalendarSlots3")]
+    [Post("/api/Doctor/CalendarSlots")]
     Task<List<ProviderSearchResponse>> DoctorCalendarSlots(
         [Body] ProviderSearchRequest request,
         [Header("Authorization")] string token);
