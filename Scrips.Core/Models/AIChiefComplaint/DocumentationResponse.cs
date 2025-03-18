68240a982ce7f68a358e2e42a53a6d2946ec0c67
Andrew Safwat <andrew@scrips.com>
2025-03-18 10:58:17 +0200

    add model for dynamic CC

---
+++ Scrips.Core/Models/AIChiefComplaint/DocumentationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Scrips.Core.Models.AIChiefComplaint;
+
+public class DocumentationResponse
+{
+    public string ChiefComplaint { get; set; }
+    public string FormattedDocumentation { get; set; }
+    public string Category { get; set; }
+    public string GenerationMethod { get; set; }
+}
---
+++ Scrips.Core/Models/AIChiefComplaint/RAGSearchRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Scrips.Core.Models.AIChiefComplaint;
+
+public class RAGSearchRequest
+{
+    public string SearchText { get; set; }
+    public string SpecialtyContext { get; set; }
+}
